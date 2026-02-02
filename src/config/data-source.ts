5b17c754240f33e299457cf1d037992c211302a3
golcar <erix.golcar@gmail.com>
2026-02-02 16:06:13 -0600

    fix DB

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -6,12 +6,10 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true, // equivalente a ddl-auto:update
+    url: process.env.DATABASE_URL,
+    synchronize: true, // 👈 luego en prod lo cambiamos
     logging: false,
-    entities: ["src/entities/*.ts"],
+    entities: ["dist/entities/*.js"],
+    migrations: [],
+    subscribers: [],
 });
