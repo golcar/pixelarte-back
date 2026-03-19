fb56368d73c495b04951c312cd5118f454962ff8
golcar <erix.golcar@gmail.com>
2026-03-18 20:13:17 -0600

    HU3 BD refactor

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,17 +1,17 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 
-dotenv.config();
+//dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    //url: process.env.DATABASE_URL,
+   // host: process.env.DB_HOST,
+    //port: Number(process.env.DB_PORT),
+    //username: process.env.DB_USER,
+    //password: process.env.DB_PASSWORD,
+    //database: process.env.DB_NAME,
+    url: process.env.DATABASE_URL,
     synchronize: true, // ✅ OK por ahora
     logging: false,
     entities: ["dist/entities/*.js"],
